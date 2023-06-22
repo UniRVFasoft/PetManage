5cd11a331ed2262bd5842f8c83c290420417515d
rhogger <rhoggerrv@gmail.com>
2023-06-21 21:03:01 -0300

    :construction: Ajustando o footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
 
-import { AppComponent } from './app.component';
-import { ServicesListComponent } from './components/services-list/services-list.component';
-import { SchedulingComponent } from './components/scheduling/scheduling.component';
+import { ServicesListComponent } from './components/services-list/services-list.component'
+import { SchedulingComponent } from './components/scheduling/scheduling.component'
 
 const routes: Routes = [
-  // { path: '', component: AppComponent },
-  { path: 'services-list', component: ServicesListComponent },
-  { path: 'scheduling', component: SchedulingComponent },
-];
+	{ path: 'services-list', component: ServicesListComponent },
+	{ path: 'scheduling', component: SchedulingComponent },
+]
 
 @NgModule({
-  declarations: [],
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+	declarations: [],
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
 export class AppRoutingModule {}
