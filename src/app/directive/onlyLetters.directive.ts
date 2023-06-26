0f4141e86383df3ebee02914d1c75091460bdcb8
rhogger <rhoggerrv@gmail.com>
2023-06-25 23:41:28 -0300

    :bug: Corrigindo bug da diretiva que não aceitava acentos e nem espaço

--- src/app/directive/onlyLetters.directive.ts
+++ src/app/directive/onlyLetters.directive.ts
@@ -7,12 +7,11 @@ export class OnlyLettersDirective {
 	constructor(private element: ElementRef) {}
 
 	@HostListener('input', ['$event'])
-	onInput(event: KeyboardEvent) {
+	onInput(event: InputEvent) {
 		const input = event.target as HTMLInputElement
-		const regex = /^[a-zA-Z]+$/
 
 		const inputValue = input.value
-		const newValue = input.value.replace(/[^a-zA-Z]+/g, '')
+		const newValue = input.value.replace(/[^a-zA-ZÀ-ÿ\s]+/g, '')
 
 		if (newValue !== inputValue) {
 			input.value = newValue
